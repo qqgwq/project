1765398ec2fbbc29fdacd8fccd027bff4bff5fb8
gwq <1305851004@qq.com>
2019-11-23 18:13:56 +0800

    create project

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  resources :projects
+  resources :teams
+  
+  as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmation: 'confirmations'
+  }
+  root 'home#index'
+end
